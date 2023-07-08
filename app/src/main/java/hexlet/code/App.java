2e73d62aa08cead64891d6be4a8dcdf4c85dfe33
Nastasiya Trusova <trusova.ab@yandex.ru>
2023-07-08 12:32:56 +0300

    add Makefile, add help option

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,33 @@
 package hexlet.code;
 
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Help.ColorScheme;
+import picocli.CommandLine.Help.Ansi.Style;
+
+@Command(name = "gendiff",
+        mixinStandardHelpOptions = true,
+        version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference.")
 public class App {
-    public static void main(String[] args) {
+
+    public static void greeting() {
         System.out.println("Hello World!");
+    }
+
+    public static void main(String[] args) {
+        ColorScheme colorScheme = new ColorScheme.Builder()
+                .commands    ()
+                .options     (Style.fg_blue)
+                .parameters  (Style.fg_blue)
+                .optionParams(Style.italic)
+                .errors      (Style.fg_red, Style.bold)
+                .stackTraces (Style.italic)
+                .applySystemProperties()
+                .build();
+
+        new CommandLine(new App())
+                .setColorScheme(colorScheme) // use this color scheme in the usage help message
+                .execute(args);
     }
 }
